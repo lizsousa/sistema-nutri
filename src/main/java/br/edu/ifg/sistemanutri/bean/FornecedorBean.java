68af92cebdaaf7809794b9fc84987d0d1d09b915
lizandra <lizandra@lizandra-HP-Stream-Notebook-PC-13>
2019-10-02 16:53:46 -0300

    Criaçao do Fornecedor, porem precisa de alguns ajustes. Ainda nao esta 100%

---
+++ src/main/java/br/edu/ifg/sistemanutri/bean/FornecedorBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifg.sistemanutri.bean;
+
+import br.edu.ifg.sistemanutri.entity.Fornecedor;
+import br.edu.ifg.sistemanutri.logic.FornecedorLogic;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class FornecedorBean extends GenericCrud<Fornecedor, FornecedorLogic>{
+    
+    @Inject
+    private FornecedorLogic logic;
+    
+    private int tipoFornecedor;
+
+    public int getTipoFornecedor() {
+        return tipoFornecedor;
+    }
+
+    public void setTipoFornecedor(int tipoFornecedor) {
+        this.tipoFornecedor = tipoFornecedor;
+    }
+    
+    @Override
+    public FornecedorLogic getLogic() {
+        return logic;
+    }
+    
+    public List<Fornecedor> getFornecedores(){
+        try {
+            return getLogic().buscar(null);
+        } catch (Exception ex) {
+            addMensagemErro(ex.getMessage());
+            return null;
+        }
+    }
+}
